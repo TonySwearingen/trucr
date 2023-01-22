d368a3af72a7359e3901489bc6311cf5d47ed69a
TonySwearingen <unlimited1974@aol.com>
2023-01-22 14:34:35 -0500

    Copy right commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Trucker Ready US Continental Road-Conditions</h1>
+          <h6>copy right: 01/22/2023</h6>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
