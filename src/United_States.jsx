650fefb309aa4a8b0bcc7802f6cc1a1a4f559b8b
TonySwearingen <unlimited1974@aol.com>
2023-01-25 13:24:20 -0500

    Divide States into Div's with some CSS

--- src/United_States.jsx
+++ src/United_States.jsx
@@ -4,106 +4,116 @@ function United_States() {
   return (
     <div>
       <div className='states'>
-        Alabama
-        <br />
-        Alaska
-        <br />
-        Arizona
-        <br />
-        Arkansas
-        <br />
-        California
-        <br />
-        Colorado
-        <br />
-        Connecticut
-        <br />
-        Delaware
-        <br />
-        Florida
-        <br />
-        Georgia
-        <br />
-        Hawaii
-        <br />
-        Idaho
-        <br />
-        Illinois
-        <br />
-        Indiana
-        <br />
-        Iowa
-        <br />
-        Kansas
-        <br />
-        Kentucky
-        <br />
-        Louisiana
-        <br />
-        Maine
-        <br />
-        Maryland
-        <br />
-        Massachusetts
-        <br />
-        Michigan
-        <br />
-        Minnesota
-        <br />
-        Mississippi
-        <br />
-        Missouri
-        <br />
-        Montana
-        <br />
-        Nebraska
-        <br />
-        Nevada
-        <br />
-        New Hampshire
-        <br />
-        New Jersey
-        <br />
-        New Mexico
-        <br />
-        New York
-        <br />
-        North Carolina
-        <br />
-        North Dakota
-        <br />
-        Ohio
-        <br />
-        Oklahoma
-        <br />
-        Oregon
-        <br />
-        Pennsylvania
-        <br />
-        Rhode Island
-        <br />
-        South Carolina
-        <br />
-        South Dakota
-        <br />
-        Tennessee
-        <br />
-        Texas
-        <br />
-        Utah
-        <br />
-        Vermont
-        <br />
-        Virginia
-        <br />
-        Washington
-        <br />
-        West Virginia
-        <br />
-        Wisconsin
-        <br />
-        Wyoming
-        <br />
+        <div className='col1'>
+          Alabama
+          <br />
+          Alaska
+          <br />
+          Arizona
+          <br />
+          Arkansas
+          <br />
+          California
+          <br />
+          Colorado
+          <br />
+          Connecticut
+          <br />
+          Delaware
+          <br />
+          Florida
+          <br />
+          Georgia
+          <br />
+        </div>
+        <div className='col2'>
+          Hawaii
+          <br />
+          Idaho
+          <br />
+          Illinois
+          <br />
+          Indiana
+          <br />
+          Iowa
+          <br />
+          Kansas
+          <br />
+          Kentucky
+          <br />
+          Louisiana
+          <br />
+          Maine
+          <br />
+          Maryland
+          <br />
+        </div>
+        <div className='col3'>
+          Massachusetts
+          <br />
+          Michigan
+          <br />
+          Minnesota
+          <br />
+          Mississippi
+          <br />
+          Missouri
+          <br />
+          Montana
+          <br />
+          Nebraska
+          <br />
+          Nevada
+          <br />
+          New Hampshire
+          <br />
+          New Jersey
+          <br />
+        </div>
+        <div className='col4'>
+          New Mexico
+          <br />
+          New York
+          <br />
+          North Carolina
+          <br />
+          North Dakota
+          <br />
+          Ohio
+          <br />
+          Oklahoma
+          <br />
+          Oregon
+          <br />
+          Pennsylvania
+          <br />
+          Rhode Island
+          <br />
+          South Carolina
+          <br />
+        </div>
+        <div className='col5'>
+          South Dakota
+          <br />
+          Tennessee
+          <br />
+          Texas
+          <br />
+          Utah
+          <br />
+          Vermont
+          <br />
+          Virginia
+          <br />
+          Washington
+          <br />
+          West Virginia
+          <br />
+          Wisconsin
+          <br />
+          Wyoming
+          <br />
+        </div>
       </div>
     </div>
   )
